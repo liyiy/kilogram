453a87dece5f7e5ccf3d714f639a01f616a5e321
liyi <liyiyu12@gmail.com>
2018-11-27 09:25:51 -0500

    get app production ready

--- frontend/components/posts/post_show.jsx
+++ frontend/components/posts/post_show.jsx
@@ -26,6 +26,8 @@ class PostShow extends React.Component {
 
   render() {
 
+    if (!this.props.post) return <></>;
+
     let deleteButton;
       if (this.props.post.currUser === this.props.post.post.poster_id) {
         deleteButton = (
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,45 @@
+
 const path = require('path');
+
+const webpack = require("webpack");
+
+const plugins = []; 
+const devPlugins = []; 
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/kilogram.jsx',
+  entry: "./frontend/<name of entry file>",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
-    rules: [
+    loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/env']
+        }
       }
     ]
   },

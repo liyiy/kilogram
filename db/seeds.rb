54690a73b567d71b51a9e46b0d931359783ab80e
liyi <liyiyu12@gmail.com>
2018-12-12 10:48:11 -0500

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all 
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+Follow.destroy_all
+Like.destroy_all
 
 User.create!(username: 'demo', password: 'demopass')
+User.create!(username: 'liyi', password: 'password')
+User.create!(username: 'kilogram', password: 'password')
+User.create!(username: 'instagram', password: 'password')
+User.create!(username: 'codingirl', password: 'password')
+User.create!(username: 'bananas', password: 'password')
+User.create!(username: 'bananas2', password: 'password')
